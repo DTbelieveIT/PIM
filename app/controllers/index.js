2f7303d6395cb0b0cc7b763fc3318111c194f916
guodongtao <gdtbelievefuture@163.com>
2017-04-16 11:20:23 +0800

    1、修改主页名字 2、配置文件中root账号添加img属性

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,7 @@
 //index page
 exports.index = function(req,res){
 	res.render('index',{
-		title:req.session.user?'Personal Information Management in TYA':'登陆页面',
+		title:req.session.user?'员工信息管理系统(TYA)':'登陆页面',
 		user:req.session.user
 	})
 }
